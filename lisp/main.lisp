0062e41987718d240eec37120d225b0a201a25ba
taichi221228 <taichi221228@icloud.com>
2024-01-18 23:56:01 +0900

    Refactor FizzBuzz logic in main.lisp
    Revised the condition checking in the FizzBuzz loop for more clarity and accuracy. The new implementation better conforms to the standard FizzBuzz rule by checking divisible by both 3 and 5.

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -1,5 +1,5 @@
 (loop for i from 1 to 100
-      do (cond ((= 0 (mod i 15)) (format t "~%FizzBuzz"))
+      do (cond ((and (= 0 (mod i 3)) (= 0 (mod i 5))) (format t "~%FizzBuzz"))
                ((= 0 (mod i 5) ) (format t "~%Buzz"))
                ((= 0 (mod i 3) ) (format t "~%Fizz"))
                (t (format t (concatenate 'string "~%" (write-to-string i))))))
