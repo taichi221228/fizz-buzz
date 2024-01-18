483c1504ce8fef83ce60c21674d658c87d87d534
taichi221228 <taichi221228@icloud.com>
2024-01-18 23:34:20 +0900

    Add "fizz-buzz" project setup and main function in Rust
    This commit adds the initial setup for the "fizz-buzz" project in Rust including the creation of .toml and lock files and setup in the .rustc_info.json file. It also includes adding the main function that currently outputs a "Hello, world!" message.

---
+++ rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a.d
@@ -0,0 +1,5 @@
+/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a: src/main.rs
+
+/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/fizz-buzz.d
@@ -0,0 +1,1 @@
+/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/fizz-buzz: /Users/taichi221228/Projects/fizz-buzz/rust/src/main.rs
