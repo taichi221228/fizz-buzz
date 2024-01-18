0dea0c26fddd1e9d138349ca7c2160c1754f27c6
taichi221228 <taichi221228@icloud.com>
2024-01-18 23:35:30 +0900

    Replace "Hello, world!" with "FizzBuzz" algorithm in Rust
    The "Hello, world!" message in main.rs has been replaced by a standard implementation of the "FizzBuzz" game. The program will now loop from 1 to 100, printing "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for multiples of both.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+
+    loop {
+        count +=  1;
+
+        if count % 15 == 0 { println!("FizzBuzz") }
+        else if count % 5 == 0 { println!("Buzz") }
+        else if count % 3 == 0 { println!("Fizz") }
+        else { println!("{}", count) }
+
+        if count == 100 { break }
+    }
 }
