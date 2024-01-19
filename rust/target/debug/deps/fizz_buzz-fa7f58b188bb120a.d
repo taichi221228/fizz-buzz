07eeccbb9ef0ecb2aad9f38c399ffcda54a2e17c
taichi221228 <taichi221228@icloud.com>
2024-01-19 16:41:03 +0900

    Remove unnecessary files from rust/target directory
    This commit removes the redundant files located in the target directory of the Rust project. These files were generated during the build but are not required for version control, hence were unnecessarily cluttering the repository. A .gitignore file was also added to prevent future tracking of such files.

--- rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a.d
+++
@@ -1,5 +0,0 @@
-/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a: src/main.rs
-
-/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/deps/fizz_buzz-fa7f58b188bb120a.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/fizz-buzz.d
+++
@@ -1,1 +0,0 @@
-/Users/taichi221228/Projects/fizz-buzz/rust/target/debug/fizz-buzz: /Users/taichi221228/Projects/fizz-buzz/rust/src/main.rs
