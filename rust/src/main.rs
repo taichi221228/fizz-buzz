10dc474060619cfe4efd849cb6dc2c810868c1e1
taichi221228 <taichi221228@icloud.com>
2024-01-19 18:22:46 +0900

    Refactor FizzBuzz function in Rust
    Refactored main function to make use of a separate FizzBuzz function that returns results as a String. Extracted the logic into this standalone function for better readability and modularity. The main function's loop now simply calls this function and prints its results.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,15 @@
+fn fizz_buzz(n: i32) -> String {
+    if n % 3 == 0 && n % 5 == 0 { "FizzBuzz".to_string() }
+    else if n % 5 == 0 { "Buzz".to_string() }
+    else if n % 3 == 0 { "Fizz".to_string() }
+    else { n.to_string() }
+}
+
 fn main() {
-    let mut n = 1;
-
+    let mut i = 1;
     loop {
-        if n % 3 == 0 && n % 5 == 0 { println!("FizzBuzz") }
-        else if n % 5 == 0 { println!("Buzz") }
-        else if n % 3 == 0 { println!("Fizz") }
-        else { println!("{}", n) }
-
-        if n == 100 { break }
-
-        n += 1;
+        println!("{}", fizz_buzz(i));
+        if i == 100 { break }
+        i += 1;
     }
 }
