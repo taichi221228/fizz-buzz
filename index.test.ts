52d3ac8811dad119e26542f1bf47c90c529e7dac
taichi221228 <taichi221228@icloud.com>
2024-01-19 17:48:02 +0900

    Refactor handler function parameters in test cases
    The handler function signature has been simplified by straightly passing command sequences and optional spawn options. It changes how the test cases call the handler function, improving readability and usage simplicity. This refactoring does not affect the functionality of the tests.

--- index.test.ts
+++ index.test.ts
@@ -5,7 +5,8 @@ const ANSWER =
   "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\nBuzz";
 
 const handler = (
-  { cmds, ...options }: { cmds: string[] } & SpawnOptions.OptionsObject,
+  cmds: string[],
+  options?: SpawnOptions.OptionsObject,
 ) =>
 async () => {
   const { stdout } = spawn(cmds, options);
@@ -13,14 +14,14 @@ async () => {
   expect(output.trim()).toMatch(ANSWER);
 };
 
-test("JavaScript", handler({ cmds: ["bun", "javascript/fizz-buzz.js"] }));
+test("JavaScript", handler(["bun", "javascript/fizz-buzz.js"]));
 
-test("TypeScript", handler({ cmds: ["bun", "typescript/fizz-buzz.ts"] }));
+test("TypeScript", handler(["bun", "typescript/fizz-buzz.ts"]));
 
-test("Lisp", handler({ cmds: ["clisp", "lisp/fizz-buzz.lisp"] }));
+test("Lisp", handler(["clisp", "lisp/fizz-buzz.lisp"]));
 
-test("Haskell", handler({ cmds: ["runghc", "haskell/fizz-buzz.hs"] }));
+test("Haskell", handler(["runghc", "haskell/fizz-buzz.hs"]));
 
-test("Nim", handler({cmds: ["./nim/fizzBuzz"]}));
+test("Nim", handler(["./nim/fizzBuzz"]));
 
-test("Rust", handler({ cmds: ["./rust/target/debug/fizz-buzz"] }));
+test("Rust", handler(["./rust/target/debug/fizz-buzz"]));
