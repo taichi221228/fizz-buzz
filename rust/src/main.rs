1c09173bd6cdd17b98c07cc13b0f5315174eb952
taichi221228 <taichi221228@icloud.com>
2024-01-19 16:21:29 +0900

    Refactor variable name in FizzBuzz implementation
    Replaced the variable 'i' with 'n' in the Rust FizzBuzz program. This change does not affect the functionality but may enhance readability and consistency with coding standards.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut i = 1;
+    let mut n = 1;
 
     loop {
-        if i % 3 == 0 && i % 5 == 0 { println!("FizzBuzz") }
-        else if i % 5 == 0 { println!("Buzz") }
-        else if i % 3 == 0 { println!("Fizz") }
-        else { println!("{}", i) }
+        if n % 3 == 0 && n % 5 == 0 { println!("FizzBuzz") }
+        else if n % 5 == 0 { println!("Buzz") }
+        else if n % 3 == 0 { println!("Fizz") }
+        else { println!("{}", n) }
 
-        if i == 100 { break }
+        if n == 100 { break }
 
-        i += 1;
+        n += 1;
     }
 }
