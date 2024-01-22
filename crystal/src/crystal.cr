33f468cd0cf72e4d9b806d65c962bb21edd39f17
taichi221228 <taichi221228@icloud.com>
2024-01-22 18:18:09 +0900

    Implement FizzBuzz logic in Crystal module
    The FizzBuzz algorithm has been implemented in the Crystal module. This version of the function effectively returns "Fizz" if the number is divisible by 3, "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both. It loops through numbers from 1 to 100 to demonstrate this implementation.

--- crystal/src/crystal.cr
+++ crystal/src/crystal.cr
@@ -1,5 +1,21 @@
-# TODO: Write documentation for `Crystal`
+def fizzBuzz(n)
+  if (n % 3 == 0 && n % 5 == 0)
+    return "FizzBuzz"
+  elsif (n % 3 == 0)
+    return "Fizz"
+  elsif (n % 5 == 0)
+    return "Buzz"
+  else
+    return n
+  end
+end
+
 module Crystal
-  # TODO: Put your code here
-  puts "Hello Crystal! Version #{Crystal::VERSION}"
+  n = 1
+  while true
+    puts fizzBuzz(n)
+
+    break if (n == 100)
+    n += 1
+  end
 end
