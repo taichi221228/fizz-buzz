186c43d09200664669d8fa80529955eb4dfe2000
taichi221228 <taichi221228@icloud.com>
2024-01-26 19:49:33 +0900

    Create initial Elixir project setup
    This commit introduces the initial setup for an Elixir project. It includes the new Elixir files needed for the basic configuration of an Elixir project, like the mix.exs, README.md and script test files. Moreover, the feature for testing using Elixir's ExUnit framework has also been added.

---
+++ elixir/lib/main.ex
@@ -0,0 +1,18 @@
+defmodule Main do
+  @moduledoc """
+  Documentation for `Main`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Main.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
