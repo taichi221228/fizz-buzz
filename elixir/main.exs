1494694639b784734f3facd199620f8ce730d0b8
taichi221228 <taichi221228@icloud.com>
2024-01-28 17:00:21 +0900

    Implement FizzBuzz in the main Elixir file
    Replaced the simple "Hello World" program in the main Elixir file with an implementation of the FizzBuzz problem. The file now contains a functioning FizzBuzz program that runs from 1 to 100, printing 'Fizz' for multiples of 3, 'Buzz' for multiples of 5, and 'FizzBuzz' for multiples of both 3 and 5.

--- elixir/main.exs
+++ elixir/main.exs
@@ -1,1 +1,11 @@
-IO.puts "Hello, world!"
+defmodule Main do
+  def fizz_buzz(i) do
+    cond do rem(i, 3) == 0 && rem(i, 5) == 0 -> "FizzBuzz"
+      rem(i, 3) == 0 -> "Fizz"
+      rem(i, 5) == 0 -> "Buzz"
+      true -> i
+    end
+  end
+end
+
+for i <- 1..100, do: IO.puts Main.fizz_buzz(i)
