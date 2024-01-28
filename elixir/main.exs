2dbe921934eb38a53372f3993e37cbbd682751fd
taichi221228 <taichi221228@icloud.com>
2024-01-28 17:36:52 +0900

    Refactor FizzBuzz implementation in Elixir
    The FizzBuzz function has been optimized and made more readable with the introduction of helper functions and the pipes operator. This new implementation reduces repeated operations, streamlines conditions checks, and improves code comprehension overall. Also modified was the loop iterating from 1 to 100, making it more idiomatic by using the Enum.each function.

--- elixir/main.exs
+++ elixir/main.exs
@@ -1,11 +1,15 @@
 defmodule Main do
-  def fizz_buzz(i) do
-    cond do rem(i, 3) == 0 && rem(i, 5) == 0 -> "FizzBuzz"
-      rem(i, 3) == 0 -> "Fizz"
-      rem(i, 5) == 0 -> "Buzz"
-      true -> i
-    end
+  def fizz_buzz(n) do
+    {n, ""}
+    |> add_fizz()
+    |> add_buzz()
+    |> output()
   end
+
+  defp add_fizz({n, acc}), do: if(rem(n, 3) == 0, do: {n, acc <> "Fizz"}, else: {n, acc})
+  defp add_buzz({n, acc}), do: if(rem(n, 5) == 0, do: {n, acc <> "Buzz"}, else: {n, acc})
+  defp output({n, ""}), do: n
+  defp output({_, acc}), do: acc
 end
 
-for i <- 1..100, do: IO.puts Main.fizz_buzz(i)
+1..100 |> Enum.each(&IO.puts(Main.fizz_buzz(&1)))
