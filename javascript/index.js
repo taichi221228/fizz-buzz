2a14e8c0362e57331ff90cf8308610faeafbfc14
taichi221228 <taichi221228@icloud.com>
2024-01-28 17:06:05 +0900

    Refactor FizzBuzz function in JavaScript and TypeScript files
    The FizzBuzz function in both the JavaScript and TypeScript files has been refactored to use ternaries for clarity and brevity. The revised implementation retains the original logic, but presents it in a more readable and concise format, improving maintainability.

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,10 @@
-const fizzBuzz = (n) => {
-  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
-  else if (n % 3 === 0) return "Fizz";
-  else if (n % 5 === 0) return "Buzz";
-  else return n + "";
-};
+const fizzBuzz = (n) =>
+  (n % 3 === 0 && n % 5 === 0)
+    ? "FizzBuzz"
+    : (n % 3 === 0)
+    ? "Fizz"
+    : (n % 5 === 0)
+    ? "Buzz"
+    : `${n}`;
 
 [...Array(100).keys()].forEach((i) => console.log(fizzBuzz(i + 1)));
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,10 +1,12 @@
 type FizzBuzz = (n: number) => string;
 
-const fizzBuzz: FizzBuzz = (n) => {
-  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
-  else if (n % 3 === 0) return "Fizz";
-  else if (n % 5 === 0) return "Buzz";
-  else return n + "";
-};
+const fizzBuzz: FizzBuzz = (n) =>
+  (n % 3 === 0 && n % 5 === 0)
+    ? "FizzBuzz"
+    : (n % 3 === 0)
+    ? "Fizz"
+    : (n % 5 === 0)
+    ? "Buzz"
+    : `${n}`;
 
 [...Array(100).keys()].forEach((i) => console.log(fizzBuzz(i + 1)));
