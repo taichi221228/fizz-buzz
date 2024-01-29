60f2e0ec40f2a17cdf542546a39697c002dc4949
taichi221228 <taichi221228@icloud.com>
2024-01-29 22:00:32 +0900

    Optimize FizzBuzz condition in Haskell code
    This commit simplifies the primary condition for 'FizzBuzz' in the main.hs Haskell file, using modulus 15 instead of two different modulus operations. Furthermore, a minor adjustment has been made to the main function for generating the first 100 numbers.

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,9 +1,9 @@
 fizzBuzz :: Int -> String
 fizzBuzz n
-  | n `mod` 3 == 0 && n `mod` 5 == 0 = "FizzBuzz"
+  | n `mod` 15 == 0 = "FizzBuzz"
   | n `mod` 3 == 0 = "Fizz"
   | n `mod` 5 == 0 = "Buzz"
   | otherwise = show n
 
 main :: IO ()
-main = mapM_ (putStrLn . fizzBuzz) [1..100]
+main = mapM_ (putStrLn . fizzBuzz) $ take 100 [1..]
