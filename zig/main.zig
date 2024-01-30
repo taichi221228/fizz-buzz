2003c2d2aba7241280fb133616196f7e93acb60f
taichi221228 <taichi221228@icloud.com>
2024-01-30 23:05:43 +0900

    Refactor FizzBuzz function in Zig file
    The FizzBuzz function in Zig has been refactored to check for divisibility by 15 instead of checking separately for 3 and 5, enhancing code readability. In addition, variable names were simplified from "allocator" to "alloc", improving brevity. The main function has also been amended to generate the 1-100 range directly in the for-loop statement, increasing code conciseness.

--- zig/main.zig
+++ zig/main.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
 
-fn fizz_buzz(allocator: std.mem.Allocator, n: u64) ![]const u8 {
-    if (n % 3 == 0 and n % 5 == 0) {
-        return allocator.dupe(u8, "FizzBuzz");
+fn fizz_buzz(alloc: std.mem.Allocator, n: u64) ![]const u8 {
+    if (n % 15 == 0) {
+        return alloc.dupe(u8, "FizzBuzz");
     } else if (n % 3 == 0) {
-        return allocator.dupe(u8, "Fizz");
+        return alloc.dupe(u8, "Fizz");
     } else if (n % 5 == 0) {
-        return allocator.dupe(u8, "Buzz");
+        return alloc.dupe(u8, "Buzz");
     } else {
-        return std.fmt.allocPrint(allocator, "{}", .{n});
+        return std.fmt.allocPrint(alloc, "{}", .{n});
     }
 }
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    for (0..100) |i| {
-        var n = i + 1;
-
-        const result = try fizz_buzz(allocator, n);
+    for (1..101) |i| {
+        const result = try fizz_buzz(allocator, i);
         defer allocator.free(result);
         try std.io.getStdOut().writeAll(result);
         try std.io.getStdOut().writeAll("\n");
