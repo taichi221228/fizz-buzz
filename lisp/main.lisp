46069567f1fc96cee3074cd4dbbd4ba538c46e75
taichi221228 <taichi221228@icloud.com>
2024-01-30 23:04:38 +0900

    Optimize FizzBuzz condition in Lisp file
    This commit refines the condition for generating 'FizzBuzz' in main.lisp. The primary condition replaced two separate modulus checks with a single check for modulus 15, improving efficiency and readability of the code.

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -1,7 +1,7 @@
 (defun fizzbuzz (n)
-  (cond ((and (= 0 (mod n 3)) (= 0 (mod n 5))) "FizzBuzz")
-        ((= 0 (mod n 5) ) "Buzz")
-        ((= 0 (mod n 3) ) "Fizz")
+  (cond ((= 0 (mod n 15)) "FizzBuzz")
+        ((= 0 (mod n 5)) "Buzz")
+        ((= 0 (mod n 3)) "Fizz")
         (t n)))
 
 (loop for i from 1 to 100
