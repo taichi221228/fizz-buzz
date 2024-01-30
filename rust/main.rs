ece6f69e6783de9222b92e09e53a27fb9d47f498
taichi221228 <taichi221228@icloud.com>
2024-01-30 23:05:27 +0900

    Refactor FizzBuzz function in Rust file
    The FizzBuzz function in Rust has been refactored to use a match statement instead of multiple if/else statements. This change increases the efficiency and readability of the code by replacing multiple modulus checks with a tuple match for moduli of 3 and 5. Additionally, the main function has been simplified to use a range and for_each loop.

--- rust/main.rs
+++ rust/main.rs
@@ -1,16 +1,12 @@
 fn fizz_buzz(n: i32) -> String {
-    if n % 3 == 0 && n % 5 == 0 { "FizzBuzz".to_string() }
-    else if n % 5 == 0 { "Buzz".to_string() }
-    else if n % 3 == 0 { "Fizz".to_string() }
-    else { n.to_string() }
+    match (n % 3, n % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        _ => n.to_string()
+    }
 }
 
 fn main() {
-    let mut i = 1;
-    loop {
-        println!("{}", fizz_buzz(i));
-
-        if i == 100 { break }
-        else { i += 1 }
-    }
+    (1..=100).for_each(|i| println!("{}", fizz_buzz(i)));
 }
